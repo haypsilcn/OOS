7845915ee80540c9296cc08868202c0e73aa04a5
haypsilcn <yennguyen1727@gmail.com>
2021-12-07 00:01:48 +0100

    finished Aufgabe1

--- Praktikum04/src/Main.java
+++ Praktikum04/src/Main.java
@@ -10,28 +10,71 @@ import java.util.List;
 
 
 public class Main {
-    public static void main (String[] args) throws AccountAlreadyExistsException, TransactionAlreadyExistException, AccountDoesNotExistException, TransactionDoesNotExistException, IOException {
+    public static void main(String[] args) throws AccountAlreadyExistsException, TransactionAlreadyExistException, AccountDoesNotExistException, TransactionDoesNotExistException, IOException {
 
 
         PrivateBank deutscheBank = new PrivateBank("Deutsche Bank", "Deutsche Bank", 0.25, 0.3);
+        try {
+            deutscheBank.createAccount("Molziles", List.of(
+                    new Payment("12.03.2008", "Payment", 321),
+                    new Payment("23.09.1897", "Payment", -2500, 0.8, 0.5),
+                    new OutcomingTransfer("03.03.2000", "OutcomingTransfer", 80, "Molziles", "Elixir")
+            ));
+        } catch (AccountAlreadyExistsException e) {
+            System.out.println(e);
+        }
+
+        try {
+            deutscheBank.createAccount("Elixir", List.of(
+                    new Payment("22.06.1998", "Payment", 435, 0., 0.),
+                    new IncomingTransfer("03.03.2000", "IncomingTransfer", 80, "Molziles", "Elixir"),
+                    new Payment("05.08.2022", "Payment", -118, 0., 0.),
+                    new OutcomingTransfer("15.04.1990", "OutcomingTransfer", 185, "Elixir", "Vaio"),
+                    new OutcomingTransfer("30.07.2020", "OutcomingTransfer", 1890, "Elixir", "Booth")
+            ));
+        } catch (AccountAlreadyExistsException e) {
+            System.out.println(e);
+        }
+
+        try {
+            deutscheBank.createAccount("Hagen");
+        } catch (AccountAlreadyExistsException e) {
+            System.out.println(e);
+        }
+
+        try {
+            deutscheBank.removeTransaction("Hagen", new Payment("19.01.2011", "Payment", -789, 0.9, 0.25));
+        } catch (TransactionDoesNotExistException e) {
+            System.out.println(e);
+        }
+
+        try {
+            deutscheBank.addTransaction("Hagen", new Payment("19.01.2011", "Payment", -789, 0.9, 0.25));
+        } catch (TransactionAlreadyExistException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("\n" + deutscheBank);
+
+
+
+        PrivateBank sparkasse = new PrivateBank("Sparkasse", "Sparkasse", 0.11, 0.05);
+        try {
+            sparkasse.addTransaction("Hagen", new Payment("19.01.2011", "Payment", -789, 0.9, 0.25));
+        } catch (AccountDoesNotExistException e) {
+            System.out.println(e);
+        }
+
+        try {
+            sparkasse.createAccount("Rehman");
+        } catch (AccountAlreadyExistsException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("\n" + sparkasse);
+
+
         PrivateBank aachenerBank = new PrivateBank("Aachener Bank", "Aachen", 0.11, 0.26);
-        PrivateBank copiedAachenerBank = new PrivateBank(aachenerBank);
-
-        deutscheBank.createAccount("Molziles", List.of(
-                new Payment("12.03.2008", "Payment", 321),
-                new Payment("23.09.1897", "Payment", -2500, 0.8, 0.5),
-                new OutcomingTransfer("03.03.2000", "OutcomingTransfer", 80, "Molziles", "Elixir")
-        ));
-        deutscheBank.createAccount("Elixir", List.of(
-                new Payment("22.06.1998", "Payment", 435, 0., 0.),
-                new IncomingTransfer("03.03.2000", "IncomingTransfer", 80, "Molziles", "Elixir"),
-                new Payment("05.08.2022", "Payment", -118, 0., 0.),
-                new OutcomingTransfer("15.04.1990", "OutcomingTransfer", 185, "Elixir", "Vaio"),
-                new OutcomingTransfer("30.07.2020", "OutcomingTransfer", 1890, "Elixir", "Booth")
-        ));
-        deutscheBank.createAccount("Hagen");
-        deutscheBank.addTransaction("Hagen", new Payment("19.01.2011", "Payment", -789, 0.9, 0.25));
-
-
+        System.out.println(aachenerBank);
     }
 }
--- Praktikum04/src/bank/CustomDe_Serializer.java
+++ Praktikum04/src/bank/CustomDe_Serializer.java
@@ -2,12 +2,112 @@ package bank;
 
 import com.google.gson.*;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class CustomDe_Serializer implements JsonSerializer<Transaction>, JsonDeserializer<Transaction> {
 
     @Override
     public Transaction deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+
+       /* try {
+            Reader reader = Files.newBufferedReader(Paths.get("src/PersistenceSample.json"));
+            JsonArray parser = JsonParser.parseReader(reader).getAsJsonArray();
+//            jsonElement.getAsJsonArray().addAll(parser);
+
+            for (jsonElement : parser.getAsJsonArray()) {
+                JsonObject obj = jsonElement.getAsJsonObject();
+            }
+
+            System.out.println(jsonElement);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        /*JsonArray jsonArray = (JsonArray) JsonParser.parseString(jsonElement.toString());
+
+
+        JsonObject jsonObject = jsonArray.getAsJsonObject();
+
+//        System.out.println(jsonObject.getClass());
+//        System.out.println(jsonObject.get("CLASSNAME").getAsString());
+
+        if (jsonObject.get("CLASSNAME").getAsString().equals("Payment"))
+            System.out.println("testing");
+        return new Payment(
+                jsonObject.get("date").getAsString(),
+                jsonObject.get("description").getAsString(),
+                jsonObject.get("amount").getAsDouble(),
+                jsonObject.get("incomingInterest").getAsDouble(),
+                jsonObject.get("outcomingInterest").getAsDouble()
+        ) ;*/
+        /*|| new IncomingTransfer(
+                jsonObject.get("date").getAsString(),
+                jsonObject.get("description").getAsString(),
+                jsonObject.get("amount").getAsDouble(),
+                jsonObject.get("sender").getAsString(),
+                jsonObject.get("recipient").getAsString()
+        );*/
+
+//        return null;
+
+//        JsonObject jsonObj = jsonElement.getAsJsonObject();
+//        JsonObject jsonObject = jsonObj.getAsJsonObject();
+
+        /*Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try (Reader reader = new FileReader("src/PersistenceSample.json")) {
+            jsonElement = gson.fromJson(reader, JsonElement.class);
+
+            String jsonInString = gson.toJson(jsonElement);
+
+            System.out.println(jsonInString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        */
+
+        /*return new Payment(
+                jsonObject.get("date").getAsString(),
+                jsonObject.get("description").getAsString(),
+                jsonObject.get("amount").getAsDouble(),
+                jsonObject.get("incomingInterest").getAsDouble(),
+                jsonObject.get("outcomingInterest").getAsDouble()
+        );*/
+
+//        JsonArray parser = JsonParser.
+
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+        if (jsonObject.get("CLASSNAME").getAsString().equals("Payment"))
+            return new Payment(
+                    jsonObject.get("date").getAsString(),
+                    jsonObject.get("description").getAsString(),
+                    jsonObject.get("amount").getAsDouble(),
+                    jsonObject.get("incomingInterest").getAsDouble(),
+                    jsonObject.get("outcomingInterest").getAsDouble());
+
+        else if (jsonObject.get("CLASSNAME").getAsString().equals("IncomingTransfer"))
+            new IncomingTransfer(
+                    jsonObject.get("date").getAsString(),
+                    jsonObject.get("description").getAsString(),
+                    jsonObject.get("amount").getAsDouble(),
+                    jsonObject.get("sender").getAsString(),
+                    jsonObject.get("recipient").getAsString()
+            );
+        else if (jsonObject.get("CLASSNAME").getAsString().equals("IncomingTransfer"))
+            return new OutcomingTransfer(
+                    jsonObject.get("date").getAsString(),
+                    jsonObject.get("description").getAsString(),
+                    jsonObject.get("amount").getAsDouble(),
+                    jsonObject.get("sender").getAsString(),
+                    jsonObject.get("recipient").getAsString()
+            );
         return null;
     }
 
